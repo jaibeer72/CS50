5a6f5838561f8721526c66be37055974ab51c31e
Jaibeer  S Dugal <jaibeer72@gmail.com>
2019-04-28 15:51:09 +0530

    Basics of Python

--- Hello.py
+++ Hello.py
@@ -1,2 +1,19 @@
-name=input()
-print(f"Hello,{name}!")
+def square(x): 
+    return x*x
+
+def main():
+    for i in range(10): 
+        print("{} squared is {}".format(i,square(i)))
+
+if __name__ == "__main__": #this basically says only run this main when you are running this file
+    main()                  #and not when it is importing some function to some other files 
+
+
+class Point: 
+    def __init__(self,x,y):
+        self.x= x
+        self.y= y
+
+point=Point(3,5)
+print(point.x)
+
